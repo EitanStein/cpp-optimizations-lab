931271ddb209c1957f75f22d632f0665df96fae6
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-06-11 15:55:52 +0000

    update complex return types lab with results as comments and the better logging method

--- lifetime-lab/lifetime_complex_return_type_lab.hpp
+++ lifetime-lab/lifetime_complex_return_type_lab.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "lifetime.hpp"
+#include "../utils/logger.hpp"
 #include <utility>
 #include <expected>
 #include <string_view>
@@ -10,78 +11,108 @@
 
 namespace ComplexReturnType {
     std::optional<std::pair<int, Lifetime>> getLife1(){
-        puts("Lifetime l; return std::pair{1, std::move(l)};");
-        Lifetime l;
-        return std::pair{1, std::move(l)};
+        Lifetime l; // 1 ctor
+        return std::pair{1, std::move(l)}; // 2 move ctors
     }
 
     std::optional<std::pair<int, Lifetime>> getLife2(){
-        puts("return std::optional<std::pair<int, Lifetime>>{1, Lifetime{}};");
-        return std::optional<std::pair<int, Lifetime>>{{1, Lifetime{}}};
+        return std::optional<std::pair<int, Lifetime>>{{1, Lifetime{}}}; // 1 ctor + 2 move ctors
     }
 
     std::optional<std::pair<int, Lifetime>> getLife3(){
-        puts("return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, Lifetime{}};");
-        return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, Lifetime{}};
+        return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, Lifetime{}}; // 1 ctor + 1 move ctor
     }
 
     std::optional<std::pair<int, Lifetime>> getLife4(){
-        puts("return std::optional<std::pair<int, Lifetime>>{std::in_place, std::piecewise_construct, std::forward_as_tuple(1), std::forward_as_tuple()};");
+        // 1 ctor (constructs in destination)
         return std::optional<std::pair<int, Lifetime>>{std::in_place, std::piecewise_construct, std::forward_as_tuple(1), std::forward_as_tuple()};
     }
 
     std::optional<std::pair<int, Lifetime>> getLife5(){
-        puts("return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, std::move(l)};");
-        Lifetime l;
-        return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, std::move(l)};
+        Lifetime l; // ctor
+        return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, std::move(l)}; // move ctor
     }
 
     std::optional<std::pair<int, Lifetime>> getLife6(){
-        puts("return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, l};");
-        Lifetime l;
-        return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, l};
+        Lifetime l; // ctor
+        return std::optional<std::pair<int, Lifetime>>{std::in_place, 1, l}; // copy ctor
     }
+
+    std::optional<std::pair<int, Lifetime>> getLife7(){
+        Lifetime l; // ctor
+        return std::optional<std::pair<int, Lifetime>>{{1, l}}; // copy ctor + move ctor
+    }
+
+    std::pair<int, Lifetime> getLife8(){
+        return {1, {}}; // ctor + move ctor
+    }
+
+    std::pair<int, Lifetime> getLife9(){
+        std::pair<int, Lifetime> named_pair; // ctor
+        named_pair.first = 1;
+        return named_pair; // NVRO
+    }
+
+    std::optional<Lifetime> getLife10(){
+        Lifetime l; // ctor
+        return l; // move ctor (into optional)
+    }
+
+    std::optional<Lifetime> getLife11(){
+        std::optional<Lifetime> l;
+        l.emplace(); // ctor
+        return l; // NRVO
+    }
+
+    std::optional<std::pair<int, Lifetime>> getLife12(){
+        std::optional<std::pair<int, Lifetime>> result;
+        result.emplace(std::piecewise_construct, std::forward_as_tuple(1), std::forward_as_tuple()); // ctor
+        return result; // NVRO
+    }
+
 
 
     void lifetime_and_complex_return_type_demo(){
         puts("\nlifetime_and_lambda_demo2");
         puts("complex return type std::optiona<std::pair>\n");
 
-        puts("std::optional<std::pair<int, Lifetime>> opt_l = getLife1(); :");
         {
-            std::optional<std::pair<int, Lifetime>> opt_l = getLife1();
+            std::optional<std::pair<int, Lifetime>> opt_l = CALL_AND_PRINT(getLife1());  
         }
-        puts("");
-
-        puts("std::optional<std::pair<int, Lifetime>> opt_l = getLife2(); :");
         {
-            std::optional<std::pair<int, Lifetime>> opt_l = getLife2();
+            std::optional<std::pair<int, Lifetime>> opt_l = CALL_AND_PRINT(getLife2());
         }
-        puts("");
-
-        puts("std::optional<std::pair<int, Lifetime>> opt_l = getLife3(); :");
         {
-            std::optional<std::pair<int, Lifetime>> opt_l = getLife3();
+            std::optional<std::pair<int, Lifetime>> opt_l = CALL_AND_PRINT(getLife3());
         }
-        puts("");
-
-        puts("std::optional<std::pair<int, Lifetime>> opt_l = getLife4(); :");
         {
-            std::optional<std::pair<int, Lifetime>> opt_l = getLife4();
+            std::optional<std::pair<int, Lifetime>> opt_l = CALL_AND_PRINT(getLife4());
         }
-        puts("");
-
-        puts("std::optional<std::pair<int, Lifetime>> opt_l = getLife5(); :");
         {
-            std::optional<std::pair<int, Lifetime>> opt_l = getLife5();
+            std::optional<std::pair<int, Lifetime>> opt_l = CALL_AND_PRINT(getLife5());
         }
-        puts("");
-
-        puts("std::optional<std::pair<int, Lifetime>> opt_l = getLife6(); :");
         {
-            std::optional<std::pair<int, Lifetime>> opt_l = getLife6();
+            std::optional<std::pair<int, Lifetime>> opt_l = CALL_AND_PRINT(getLife6());
         }
-        puts("");
+        {
+            std::optional<std::pair<int, Lifetime>> opt_l = CALL_AND_PRINT(getLife7());
+        }
+        {
+            std::pair<int, Lifetime> pair_l = CALL_AND_PRINT(getLife8());
+        }
+        {
+            std::pair<int, Lifetime> pair_l = CALL_AND_PRINT(getLife9());
+        }
+        {
+            std::optional<Lifetime> opt_l = CALL_AND_PRINT(getLife10());
+        }
+        {
+            std::optional<Lifetime> opt_l = CALL_AND_PRINT(getLife11());
+        }
+        {
+            std::optional<std::pair<int, Lifetime>> opt_l = CALL_AND_PRINT(getLife12());
+        }
+        
     }
 
 }
